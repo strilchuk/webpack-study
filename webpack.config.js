d799afa94962a58a7d7185ba364c4aa4a37b4357
strilchuk <strilchukalexander@gmail.com>
2021-07-22 22:55:02 +0300

    copy webpack plugin and favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -28,6 +29,14 @@ module.exports = {
             template: './index.html'
         }),
         new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
+        })
     ],
     resolve: {
         extensions: ['.js', '.json', '.png'],

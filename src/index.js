e089c84bb133b2031a787aa4d02d485e479ecf78
strilchuk <strilchukalexander@gmail.com>
2021-07-22 21:52:18 +0300

    add jquery

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import * as $ from 'jquery'
 import Post from "@models/Post"
 import '@/styles/styles.css'
 import json from '@/assets/json.json'
 import WebpackLogo from '@/assets/webpack-logo.png'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
+
+$('pre').html(post.toString());
 
 console.log('Post to string:', post.toString())
 
